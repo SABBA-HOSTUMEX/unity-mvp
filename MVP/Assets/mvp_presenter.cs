55116ec6dd3e6e93033092392a13686a6a19432d
a664104797 <a664104797@gmail.com>
2023-10-29 16:24:54 +0800

    mvp practice upload

---
+++ MVP/Assets/mvp_presenter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mvp_presenter : MonoBehaviour
+{
+    private mvp_view mvpview;
+    private mvp_model mvpmodel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mvpview = GetComponent<mvp_view>(); // 获取视图组件
+
+        // 订阅视图的事件
+        mvpview.IncreaseHealthEvent += IncreaseHealth;
+        mvpview.DecreaseHealthEvent += DecreaseHealth;
+
+        // Initialize the model and update the UI with the initial health value
+        mvpmodel = new mvp_model(); // 你需要实现 mvp_model 类
+        mvpview.UpdateHealth(mvpmodel.Health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 这里可以处理其他逻辑
+    }
+
+    // Method to handle an increase in health
+    private void IncreaseHealth()
+    {
+        mvpmodel.Health += 10;
+        mvpview.UpdateHealth(mvpmodel.Health);
+    }
+
+    // Method to handle a decrease in health
+    private void DecreaseHealth()
+    {
+        mvpmodel.Health -= 10;
+        mvpview.UpdateHealth(mvpmodel.Health);
+    }
+}
---
+++ MVP/Assets/mvp_view.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mvp_view : MonoBehaviour
+{
+    public event System.Action IncreaseHealthEvent; // 用于通知 Presenter 增加健康值的事件
+    public event System.Action DecreaseHealthEvent; // 用于通知 Presenter 减少健康值的事件
+
+    public Button btninc; // "Increase" 按钮
+    public Button btndec; // "Decrease" 按钮
+    public Text textshow; // 用于显示信息的 Text 组件
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 当按钮被点击时，触发相应的事件
+        btninc.onClick.AddListener(OnIncreaseHealthClicked);
+        btndec.onClick.AddListener(OnDecreaseHealthClicked);
+    }
+
+    // Method to handle "Increase" button click
+    private void OnIncreaseHealthClicked()
+    {
+        IncreaseHealthEvent?.Invoke(); // 触发增加健康值事件
+    }
+
+    // Method to handle "Decrease" button click
+    private void OnDecreaseHealthClicked()
+    {
+        DecreaseHealthEvent?.Invoke(); // 触发减少健康值事件
+    }
+
+    // Method to update the displayed health value
+    public void UpdateHealth(int Health)
+    {
+        textshow.text = "Health: " + Health;
+    }
+}
